7ef8495c885e057d5f2d882c7ba326f94b0dd720
Pawel <smrpawel@gmail.com>
2023-01-06 23:00:42 +0100

    added page change

---
+++ rijksmuseum/src/components/PageChange/PageChange.js
@@ -0,0 +1,19 @@
+import "./PageChange.css";
+
+const PageChange = (props) =>{
+    console.log(props.pageCounter)
+    return(
+        <section className="page">
+            <div className="page__counter">
+                <p>{props.pageCounter[0]?.value || "loading..."}</p>
+            </div>
+            <div className="page__arrows">
+                <i onClick={props.prevPage} className="fa-solid fa-angle-left page__arrowLeft"></i>
+                <i onClick={props.nextPage} className="fa-solid fa-angle-right page__arrowRight"></i>
+            </div>
+            
+        </section>
+    );
+}
+
+export default PageChange;
