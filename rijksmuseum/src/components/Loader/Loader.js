e55b203d6e7014069e791ca4c4c5534cb4e279ff
Pawel <smrpawel@gmail.com>
2023-01-06 22:59:57 +0100

    added loading circle

---
+++ rijksmuseum/src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import "./Loader.css";
+const Loader = () =>{
+    return(
+    <div class="loadingio-spinner-rolling-5o9mbploe8j">
+        <div class="ldio-103mxcineswe">
+            <div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Loader;
